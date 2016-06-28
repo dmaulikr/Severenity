d5f67fbc0e803382e672a8fac5111f52432b57f9
gorynalexander <goryn.alexander@gmail.com>
2016-06-28 22:58:43 +0300

    Test commit

--- app/src/main/java/com/nosad/sample/helpers/GoogleApiHelper.java
+++ app/src/main/java/com/nosad/sample/helpers/GoogleApiHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -62,6 +63,7 @@ public class GoogleApiHelper implements GoogleApiClient.ConnectionCallbacks, Goo
         //You are connected do what ever you want
         //Like i get last known location
         broadcastConnectedState();
+        // test
     }
 
     private void broadcastConnectedState() {
@@ -74,10 +76,12 @@ public class GoogleApiHelper implements GoogleApiClient.ConnectionCallbacks, Goo
     public void onConnectionSuspended(int i) {
         Log.d(TAG, "onConnectionSuspended: googleApiClient.connect()");
         mGoogleApiClient.connect();
+
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.d(TAG, "onConnectionFailed: connectionResult.toString() = " + connectionResult.toString());
+
     }
 }
