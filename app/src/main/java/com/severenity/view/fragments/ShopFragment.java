3986161ddea1b97ddc8ecb4f791af50c85848fb8
Oleg Novosad <adminnovik@gmail.com>
2016-07-29 18:17:42 +0300

    Removed mapbox as there is a separate branch to it. Added hardware acceleration for better performance. Changed starting screen view.

--- app/src/main/java/com/severenity/view/fragments/ShopFragment.java
+++ app/src/main/java/com/severenity/view/fragments/ShopFragment.java
@@ -6,18 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.mapbox.mapboxsdk.constants.Style;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.severenity.R;
-import com.severenity.utils.common.Constants;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ShopFragment extends Fragment {
-//    private MapView mapView;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -28,16 +22,6 @@ public class ShopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_shop, container, false);
-//        mapView = (MapView) view.findViewById(R.id.mapView);
-//        mapView.onCreate(savedInstanceState);
-//        mapView.getMapAsync(new OnMapReadyCallback() {
-//            @Override
-//            public void onMapReady(MapboxMap mapboxMap) {
-//                mapboxMap.setMaxZoom(Constants.MAX_ZOOM_LEVEL);
-//                mapboxMap.setMinZoom(Constants.MIN_ZOOM_LEVEL);
-//                mapboxMap.setStyleUrl(Style.DARK);
-//            }
-//        });
         return view;
     }
 
