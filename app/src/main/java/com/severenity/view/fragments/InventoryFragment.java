83bfec8124c166518b050d75f6e5044cc31cca5d
gorynalexander <goryn.alexander@gmail.com>
2016-09-04 00:05:57 +0300

    Added chips info to string file

--- app/src/main/java/com/severenity/view/fragments/InventoryFragment.java
+++ app/src/main/java/com/severenity/view/fragments/InventoryFragment.java
@@ -1,13 +1,18 @@
 package com.severenity.view.fragments;
 
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.severenity.R;
 import com.severenity.entity.chip.Chip;
@@ -21,15 +26,9 @@ import java.util.Arrays;
  * A simple {@link Fragment} subclass.
  */
 public class InventoryFragment extends Fragment {
+    private Context context;
     private RecyclerView rvInventory;
-    private ArrayList<Chip> chips = new ArrayList<>(Arrays.asList(
-            new Chip(Chip.ChipType.CapturePlace, "Captures place to collect data that improves your implant.", 0, Chip.Rarity.Common),
-            new Chip(Chip.ChipType.CapturePlayer, "Captures player to steal data that improves your implant.", 0, Chip.Rarity.Rare),
-            new Chip(Chip.ChipType.Remove, "Removes player who has captured from captured object.", 1, Chip.Rarity.Uncommon),
-            new Chip(Chip.ChipType.Defend, "Defends your implant from next incoming attacking signals.", 0, Chip.Rarity.Common),
-            new Chip(Chip.ChipType.Attack, "Deals damage to the selected implant.", 2, Chip.Rarity.Common),
-            new Chip(Chip.ChipType.Invisibility, "Puts invisibility over the object so it disappears from the map.", 0, Chip.Rarity.Rare)
-    ));
+    private ArrayList<Chip> chips;
 
     public InventoryFragment() {
         // Required empty public constructor
@@ -40,13 +39,33 @@ public class InventoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inventory, container, false);
-
+        createChips();
         rvInventory = (RecyclerView) view.findViewById(R.id.rvChips);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
         rvInventory.setLayoutManager(gridLayoutManager);
         rvInventory.setAdapter(new ChipRecycleViewAdapter((MainActivity) getActivity(), chips));
 
+
+
         return view;
     }
+
+    private void createChips() {
+        chips = new ArrayList<>(Arrays.asList(
+                new Chip(Chip.ChipType.CapturePlace, getResources().getString(R.string.chip_capture_place), 0, Chip.Rarity.Common),
+                new Chip(Chip.ChipType.CapturePlayer, getResources().getString(R.string.chip_capture_player), 0, Chip.Rarity.Rare),
+                new Chip(Chip.ChipType.Remove, getResources().getString(R.string.chip_remove), 1, Chip.Rarity.Uncommon),
+                new Chip(Chip.ChipType.Defend, getResources().getString(R.string.chip_defend), 0, Chip.Rarity.Common),
+                new Chip(Chip.ChipType.Attack, getResources().getString(R.string.chip_attack), 2, Chip.Rarity.Common),
+                new Chip(Chip.ChipType.Invisibility, getResources().getString(R.string.chip_invisibility), 0, Chip.Rarity.Rare)
+        ));
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
 
 }
