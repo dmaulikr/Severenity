74c96006d35101f97ec5c3b5c5317e61e80d3733
gorynalexander <goryn.alexander@gmail.com>
2016-09-04 00:08:21 +0300

    Added chips info to string file[1]

--- app/src/main/java/com/severenity/view/fragments/InventoryFragment.java
+++ app/src/main/java/com/severenity/view/fragments/InventoryFragment.java
@@ -2,17 +2,14 @@ package com.severenity.view.fragments;
 
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.severenity.R;
 import com.severenity.entity.chip.Chip;
@@ -44,9 +41,6 @@ public class InventoryFragment extends Fragment {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
         rvInventory.setLayoutManager(gridLayoutManager);
         rvInventory.setAdapter(new ChipRecycleViewAdapter((MainActivity) getActivity(), chips));
-
-
-
         return view;
     }
 
@@ -61,11 +55,7 @@ public class InventoryFragment extends Fragment {
         ));
     }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-    }
 
 
 }
