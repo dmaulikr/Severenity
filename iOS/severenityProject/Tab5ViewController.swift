d407a2285c79d07a02f9a82d880e7f49e4b6aafe
Yura Yasinskyy <yura.yasinskyy@gmail.com>
2016-09-12 14:02:16 +0300

    iOS version initial commit

---
+++ iOS/severenityProject/Tab5ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Tab5ViewController.swift
+//  severenityProject
+//
+//  Created by Yura Yasinskyy on 12.09.16.
+//  Copyright © 2016 severenity. All rights reserved.
+//
+
+import UIKit
+
+class Tab5ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tab 5 did load");
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
