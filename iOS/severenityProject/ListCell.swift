ba42e4747d6969db142177ad86c1998213e1fd1d
Yura Yasinskyy <yura.yasinskyy@gmail.com>
2016-09-13 19:10:53 +0300

    Used Alamofire library to make server requests, recieved and parsed JSON (data is displayed in UITableView)

---
+++ iOS/severenityProject/ListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListCell.swift
+//  severenityProject
+//
+//  Created by Yura Yasinskyy on 13.09.16.
+//  Copyright © 2016 severenity. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS/severenityProject/Network Managers/LocationsServerManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationsServerManager.swift
+//  severenityProject
+//
+//  Created by Yura Yasinskyy on 13.09.16.
+//  Copyright © 2016 severenity. All rights reserved.
+//
+
+import UIKit
+
+class LocationsServerManager: NSObject {
+
+}
