16044be860a9d17998b064b2c9d8e94b43c7be78
Yura Yasinskyy <yura.yasinskyy@gmail.com>
2016-09-14 13:41:55 +0300

    Finaaalllly fixed all merging conflicts

--- iOS/severenityProject/View Controllers/ShopViewController.swift
+++ iOS/severenityProject/View Controllers/ShopViewController.swift
@@ -15,22 +15,4 @@ class Tab1ViewController: UIViewController {
         print("Tab 1 did load");
     }
 
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
 }
