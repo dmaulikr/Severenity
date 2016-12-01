768562bba3ebc53f97133bf273d61267997505d9
Yuriy Yasinskyy <yyas@softserveinc.com>
2016-12-01 14:23:56 +0200

    Notification center widget is fully functional. BTW it also allowed to implement 3D Touch gestures on home screen icon.

--- iOS/Severenity/Interactors/NavigationBarInteractor.swift
+++ iOS/Severenity/Interactors/NavigationBarInteractor.swift
@@ -31,7 +31,18 @@ class NavigationBarInteractor: NSObject {
         FacebookService.sharedInstance.getFBProfilePicture(for: fbUserID, size: .normal, completion: { (image) in
             FacebookService.sharedInstance.getFBProfileInfo (with: "me", and: { (info) in
                     self.delegate?.navigationBarInteractorDidCallPresenter(with: image, and: info)
+                    self.saveSharedData(image: image, data: info)
             })
         })
+    }
+    
+    /// Saves FB profile data to UserDefaults to share it with notification center widget-extension
+    private func saveSharedData(image: UIImage, data: Dictionary<String,String>) {
+        let userDefaults = UserDefaults(suiteName: "group.severenity.DataSharing")
+        userDefaults?.set(data, forKey: "profileData")
+        let pictureData = UIImagePNGRepresentation(image)
+        userDefaults?.set(pictureData, forKey: "profilePicture")
+        userDefaults?.synchronize()
+        Log.info(message: "FB profile info saved to UserDefaults", sender: self)
     }
 }
--- iOS/SeverenityWidget/TodayViewController.swift
+++ iOS/SeverenityWidget/TodayViewController.swift
@@ -11,14 +11,15 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
     
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var userName: UILabel!
-    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
     
     // MARK: Loading view
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        preferredContentSize = CGSize(width: 320, height: 100)
         // Do any additional setup after loading the view from its nib.
     }
     
@@ -30,7 +31,43 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
+        self.retrieveSharedData()
         completionHandler(NCUpdateResult.newData)
+    }
+    
+}
+
+extension TodayViewController {
+    
+    /// Loads FB profile data from UserDefaults shared by containing application
+    func retrieveSharedData() {
+        let userDefaults = UserDefaults(suiteName: "group.severenity.DataSharing")
+        if let data = userDefaults?.dictionary(forKey: "profileData"), let name = data["name"] as? String,
+            let imageData = userDefaults?.object(forKey: "profilePicture") as? Data {
+            nameLabel.text = name
+            levelLabel.text = levelLabel.text?.appending("Father of Severenity. God of software development. Has infinite powers.")
+            profilePicture.image = UIImage(data: imageData)?.roundedImageWithBorder(with: 4, and: #colorLiteral(red: 0.5176470588, green: 0.3411764706, blue: 0.6, alpha: 1))
+        }
+    }
+    
+}
+
+extension UIImage {
+    func roundedImageWithBorder(with width: CGFloat, and color: UIColor) -> UIImage? {
+        let square = CGSize(width: min(size.width, size.height) + width * 2, height: min(size.width, size.height) + width * 2)
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: square))
+        imageView.contentMode = .center
+        imageView.image = self
+        imageView.layer.cornerRadius = square.width/2
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = width
+        imageView.layer.borderColor = color.cgColor
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
     }
     
 }
